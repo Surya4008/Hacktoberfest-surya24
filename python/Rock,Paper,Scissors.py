e55e4024ecc0838424af03e199bf2449f3071d4e
Pallavi <nux4008@gmail.com>
2024-10-31 21:57:49 +0530

    Reduced code

--- python/Rock,Paper,Scissors.py
+++ python/Rock,Paper,Scissors.py
@@ -1,50 +1,24 @@
-from random import randint
+import random
 
-#create a list of play options
-t = ["Rock", "Paper", "Scissors"]
+def play():
+    options = ["rock", "paper", "scissors"]
+    computer_choice = random.choice(options)
+    user_choice = input("Enter rock, paper, or scissors: ").lower()
 
-#assign a random play to the computer
-computer = t[randint(0,2)]
-your_score = 0
-computer_score = 0
-#set player to False
-player = False
-print(''' + "Welcome to Rock, Paper, Scissors! Type 'quit' to exit the game." + ''')
-while player == False:
-#set player to True
-    player = input("Rock, Paper, Scissors?")
-    if player == computer:
-        print("Tie!")
-    elif player == "Rock":
-        if computer == "Paper":
-            print("You lose!", computer, "covers", player)
-            computer_score += 1
-        else:
-            print("You win!", player, "smashes", computer)
-            your_score += 1
-    elif player == "Paper":
-        if computer == "Scissors":
-            print("You lose!", computer, "cut", player)
-            computer_score += 1
-        else:
-            print("You win!", player, "covers", computer)
-            your_score += 1
-    elif player == "Scissors":
-        if computer == "Rock":
-            print("You lose...", computer, "smashes", player)
-            computer_score += 1
-        else:
-            print("You win!", player, "cut", computer)
-            your_score += 1
+    if user_choice not in options:
+        print("Invalid choice! Please choose rock, paper, or scissors.")
+        return play()  # Restart if invalid input
+
+    print(f"\nYou chose {user_choice}, and the computer chose {computer_choice}.")
+
+    if user_choice == computer_choice:
+        print("It's a tie!")
+    elif (user_choice == "rock" and computer_choice == "scissors") or \
+         (user_choice == "paper" and computer_choice == "rock") or \
+         (user_choice == "scissors" and computer_choice == "paper"):
+        print("You win!")
     else:
-        print("That's not a valid play. Check your spelling!")
-    if player == "quit":
-        print("Your score is", your_score)
-        print("The computer's score is", computer_score)
-        if your_score > computer_score:
-            print("You win!")
-        break
-    
-    #player was set to True, but we want it to be False so the loop continues
-    player = False
-    computer = t[randint(0,2)]
+        print("Computer wins!")
+
+# Run the game
+play()
